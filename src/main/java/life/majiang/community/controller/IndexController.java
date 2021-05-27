02fc0cdea4e20ac80067b4e11a321a8757f29a71
wonho <WONOHOOO@163.com>
2021-05-27 13:53:02 +0800

    分页完成

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,4 +1,6 @@
 package life.majiang.community.controller;
+import com.sun.org.glassfish.gmbal.ParameterNames;
+import life.majiang.community.dto.PaginationDTO;
 import life.majiang.community.dto.QuestionDTO;
 import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.mapper.UserMapper;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -22,7 +25,9 @@ public class IndexController {
     private QuestionService questionService;
     @GetMapping("/")
     public String Hello(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "2") Integer size){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -35,8 +40,8 @@ public class IndexController {
                     break;
                 }
             }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -14,6 +14,6 @@ public interface UserMapper {
     @Select("select * from user where token = #{token}")
     User findByToken(@Param("token") String token);
 
-    @Select("select * from user where id = #{id}")
+    @Select("select * from user where account_id = #{id}")
     User findById(@Param("id")Integer creator);
 }
