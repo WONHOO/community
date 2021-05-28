33607e634210dae6447516992925dea788d61199
wonho <WONOHOOO@163.com>
2021-05-28 14:46:27 +0800

    有问题待解决

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -1,13 +1,16 @@
 package life.majiang.community.controller;
 
 
+import life.majiang.community.dto.QuestionDTO;
 import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.model.Question;
 import life.majiang.community.model.User;
+import life.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,18 +19,34 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class PublishController {
     @Autowired
-    private QuestionMapper questionMapper;
-    @GetMapping("/publish")
-    public String publish() {
+    private QuestionService questionService;
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id") Integer id,
+                       Model model) {
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        model.addAttribute("id", question.getId());
+//        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
+
+
+
+
+//    @GetMapping("/publish")
+//    public String publish() {
+//        return "publish";
+//    }
 
     @PostMapping("/publish")
     public String doPublish(
             @RequestParam(value = "title", required = false) String title,
             @RequestParam(value = "description", required = false) String description,
             @RequestParam(value = "tag", required = false) String tag,
-            @RequestParam(value = "id", required = false) Long id,
+            @RequestParam(value = "id", required = false) Integer id,
             HttpServletRequest request,
             Model model){
         model.addAttribute("title", title);
@@ -58,30 +77,13 @@ public class PublishController {
         question.setDescription(description);
         question.setTag(tag);
         question.setCreator(user.getId());
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
-        questionMapper.create(question);
+        question.setId(id);
+        questionService.createOrUpdate(question);
         return "redirect:/";
     }
 }
 
-//
-//    @Autowired
-//    private QuestionService questionService;
-//
-//    @GetMapping("/publish/{id}")
-//    public String edit(@PathVariable(name = "id") Long id,
-//                       Model model) {
-//        QuestionDTO question = questionService.getById(id);
-//        model.addAttribute("title", question.getTitle());
-//        model.addAttribute("description", question.getDescription());
-//        model.addAttribute("tag", question.getTag());
-//        model.addAttribute("id", question.getId());
-//        model.addAttribute("tags", TagCache.get());
-//        return "publish";
-//    }
-//
-//
+
 //    @GetMapping("/publish")
 //    public String publish(Model model) {
 //        model.addAttribute("tags", TagCache.get());
--- src/main/java/life/majiang/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -92,12 +92,51 @@ public class QuestionService {
     }
 
     public QuestionDTO getById(Integer id) {
-
         Question question = questionMapper.getById(id);
         QuestionDTO questionDTO = new QuestionDTO();
         BeanUtils.copyProperties(question,questionDTO);
         User user = userMapper.findById(question.getCreator());
         questionDTO.setUser(user);
         return questionDTO;
+    }
+
+    public void createOrUpdate(Question question) {
+        if (question.getId() == null) {
+            // 创建
+            question.setGmtCreate(System.currentTimeMillis());
+            question.setGmtModified(question.getGmtCreate());
+//            question.setViewCount(0);
+//            question.setLikeCount(0);
+//            question.setCommentCount(0);
+//            question.setSticky(0);
+            questionMapper.create(question);
+        } else {
+            // 更新
+            question.setGmtModified(System.currentTimeMillis());
+            questionMapper.update(question);
+
+
+//            Question dbQuestion = questionMapper.selectByPrimaryKey(question.getId());
+//            if (dbQuestion == null) {
+//                throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
+//            }
+//
+//            if (dbQuestion.getCreator().longValue() != question.getCreator().longValue()) {
+//                throw new CustomizeException(CustomizeErrorCode.INVALID_OPERATION);
+//            }
+//
+//            Question updateQuestion = new Question();
+//            updateQuestion.setGmtModified(System.currentTimeMillis());
+//            updateQuestion.setTitle(question.getTitle());
+//            updateQuestion.setDescription(question.getDescription());
+//            updateQuestion.setTag(question.getTag());
+//            QuestionExample example = new QuestionExample();
+//            example.createCriteria()
+//                    .andIdEqualTo(question.getId());
+//            int updated = questionMapper.updateByExampleSelective(updateQuestion, example);
+//            if (updated != 1) {
+//                throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
+//            }
+        }
     }
 }
